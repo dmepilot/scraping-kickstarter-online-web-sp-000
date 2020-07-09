2298ca51afc259737798b2ea6cec947363d21c82
Dean Erickson <dmepilot@yahoo.com>
2020-07-09 17:22:26 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -17,7 +17,7 @@ def create_project_hash
   # description: project.css("p.bbcard_blurb").text
   # location: project.css("span.location-name").text
   # percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
-  
+
 end
 
 create_project_hash
